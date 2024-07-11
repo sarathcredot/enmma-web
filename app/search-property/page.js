c90afff11aa3d67d43cefb2fac6374f1b348cf8a
nabeelCredot <nabeel@credot.in>
2024-07-11 19:08:01 +0530

    updating-content

--- app/search-property/page.js
+++ app/search-property/page.js
@@ -7,7 +7,7 @@ export default function Home7() {
     return (
         <>
             <Layout headerStyle={6} footerStyle={3}>
-            <Banner3  title={'Search Property'} paragraph={'Lorem ipsum dolor sit amet consectetur. Mus et rhoncus pretium euismod pellentesque. Consectetur lacus enim eleifend vitae nibh sem.lacus enim eleifend vitae nibh sem it amet consectetur.'} bgColor={"#110B79"} fontColor={"#FFFFFF"}fontColor2={'#FFFFFF'}/>
+            <Banner3  title={'Search Property'} paragraph={''} bgColor={"#110B79"} fontColor={"#FFFFFF"}fontColor2={'#FFFFFF'}/>
             <Searchproperty />
 
           <Bannerfooter/>
